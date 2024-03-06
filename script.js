38f3e3e5b7054d7743bc4fb71a1c6f61521f78b0
Nelson-Afonso404 <nelsonafonso412@gmail.com>
2024-03-06 16:07:15 -0300

    começando o projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+const textarea = document.getElementById("textarea");
+
+function f1(e){
+    let value = e.value;
+    textarea.style.fontSize = value + "px";
+}
+function f2(e){
+    textarea.style.text
+}
